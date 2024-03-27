94f42d38bffc0b5b1c5540d535db4c664ec2a318
Muhammad Shaheryar <shaheryarashraf824@gmail.com>
2024-03-27 23:31:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+let num = "shery"
+let num1 = 34
+console.log(num+num1)
+
+console.log(typeof(num+num1))
+
+const a = {
+  name: "sheri",
+  section: 1,
+  isprinciple : false
+
+}
+a['friend '] = "usman"
+console.log(a)
+
+
+let a1 = 24;
+let b = 65;
+console.log("a++b:",a1+b)
+console.log("a-b:",a1-b)
+console.log("a**b:",a1*b)
+console.log("a/b:",a1/b)
+
+// assignment operators
+
+let c = 23;
+c+=4;
+console.log(c)
+
+
+comp1 = 34;
+comp2 = 33;
+console.log(comp1 == comp2)
+console.log(comp1 != comp2)
+
+r = 5;
+t = 45;
+console.log(r>t && r==5)
+console.log(r<t || r ==5)
+
+// input [4,6,4,7,4,7]
+function greet(){
+  console.log("hello "+ "everyone")
+
+}
+greet()
+
+
+let a2 = prompt("whats your age?")
+a = Number.parseInt(a2)
+if (a2>0){
+  alert("this is a valid age")
+}
+
+a3 = 23
+
+if (a3<0){
+  alert("you are a kid")
+
+}
+else if (a31<23){
+  alert("you are a kid ")
+
+}
+
+else if (a3<23 && a3<0){
+  alert("you cannot think of driving")
+}
+console.log("done")
+
+
+
